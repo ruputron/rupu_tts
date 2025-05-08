2a821ec05da410514433c09115bc2ef5f94b1481
ruputron <james.leathard@proton.me>
2025-05-08 09:41:06 +0100

    Add files via upload

--- build_rupu_tts.bat
+++ build_rupu_tts.bat
@@ -2,5 +2,5 @@
 :: Build Rupu TTS into a standalone .exe with custom icon and taskbar name
 pyinstaller --onefile --windowed --icon=appicon.ico --name="Rupu TTS" rupu_tts.py
 echo.
-echo ✅ Build complete. The executable is located in the 'dist' folder.
+echo  Build complete. The executable is located in the 'dist' folder.
 pause
